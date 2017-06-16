6b330f5d19747ba1b6aa47c09d19f027247179ef
SeaWar741 <juanca741@gmail.com>
2017-06-16 10:16:56 -0500

    nested3

--- Prework/semana_2/Martes/nested2.rb
+++ Prework/semana_2/Martes/nested2.rb
@@ -43,5 +43,14 @@ table = [["Rodrigo Garcia",13,"Masculino","Primero",[9, 9, 7, 6, 8]],
 
 ret2 = table.map {|row| head.zip(row).to_h }
 #se hace un zip por columna tomando como keys el head y como values table. todo se inserta dentro de un hash
-p ret2
+p ret2 ==[{"Nombre"=>"Rodrigo Garcia", "Edad"=>13, "Genero"=>"Masculino", "Grupo"=>"Primero", "Calificaciones"=>[9, 9, 7, 6, 8]}, {"Nombre"=>"Fernanda Gonzalez", "Edad"=>12, "Genero"=>"Femenino", "Grupo"=>"Tercero", "Calificaciones"=>[6, 9, 8, 6, 8]}, {"Nombre"=>"Luis Perez", "Edad"=>13, "Genero"=>"Masculino", "Grupo"=>"Primero", "Calificaciones"=>[8, 7, 7, 9, 8]}, {"Nombre"=>"Ana Espinosa", "Edad"=>14, "Genero"=>"Femenino", "Grupo"=>"Segundo", "Calificaciones"=>[9, 9, 6, 8, 8]}, {"Nombre"=>"Pablo Moran", "Edad"=>11, "Genero"=>"Masculino", "Grupo"=>"Segundo", "Calificaciones"=>[7, 8, 9, 9, 8]}]
 #se imprime el ret2
+p ret2[0] == {"Nombre"=>"Rodrigo Garcia", "Edad"=>13, "Genero"=>"Masculino", "Grupo"=>"Primero", "Calificaciones"=>[9, 9, 7, 6, 8]}
+
+p ret2[1]["Nombre"] == "Fernanda Gonzalez"
+
+p ret2[3]["Grupo"] == "Segundo"
+
+p ret2[4]["Genero"] == "Masculino"
+
+p ret2[2]["Edad"] == 13
