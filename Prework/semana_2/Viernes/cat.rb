1ed6ffd50be78f76e1ffb0e91a194019517f6db1
SeaWar741 <juanca741@gmail.com>
2017-06-21 18:36:46 -0500

    Bootstrap

---
+++ Prework/semana_2/Viernes/cat.rb
@@ -0,0 +1,24 @@
+class Cat
+  CAT_YEARS = 7
+
+  attr_accessor :name, :age00000000000
+
+  def initialize(age)
+    @age = age 
+  end
+
+  def es_mayor_que?(arg)
+    age > arg.age
+  end
+
+protected
+  def age
+    @age * CAT_YEARS
+  end
+end
+
+#test
+katty = Cat.new(2)
+peto = Cat.new(4)
+p katty.es_mayor_que?(peto) == false
+#=>true
--- Prework/semana_2/Viernes/stair.rb
+++ Prework/semana_2/Viernes/stair.rb
@@ -1,11 +1,24 @@
-class Stair 
+class Stair
+	attr_accessor :material,:size
+	#se modifica y lee el material y el size dentro del codigo
+
 	def initialize (material,size)
-	@material = material
-	@size = size
+		@material = material
+		@size = size
+		#se inicializa tanto el material como el size
 	end
 
-	def larger_size_than?
-		c
+	def larger_size_than?(material2)
+		size > material2.size
+		#se compara el material uno con el size del material 2
+	end
+
+	protected
+	#zona protegida del codigo, se puede accesar pero no modificar
+
+	def size
+		@size
+		#el tamaño
 	end
 end
 #test
