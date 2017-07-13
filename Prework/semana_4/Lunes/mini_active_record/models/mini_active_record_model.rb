1fd0e82ef575c721b1f32a9d53c2fc32bd96d3e2
SeaWar741 <juanca741@gmail.com>
2017-07-13 13:47:49 -0500

    Mini active record v2 intento 1 funcional

--- Prework/semana_4/Lunes/mini_active_record/models/mini_active_record_model.rb
+++ Prework/semana_4/Lunes/mini_active_record/models/mini_active_record_model.rb
@@ -3,22 +3,76 @@ module MiniActiveRecord
   class NotConnectedError < StandardError; end
 
   class Model
-    attr_reader :attributes, :old_attributes
 
-    def initialize(attributes = {})
-      attributes.symbolize_keys!
-      raise_error_if_invalid_attribute!(attributes.keys)
+    #Insert_1
 
-      # This defines the value even if it's not present in attributes
-      @attributes = {}
+  def self.create(attributes)
+    record = self.new(attributes)
+    record.save
 
-      Chef.attribute_names.each do |name|
-        @attributes[name] = attributes[name]
-      end
+    record
+  end
 
-      @old_attributes = @attributes.dup
+  def self.where(query, *args)
+    MiniActiveRecord::Model.execute("SELECT * FROM chefs WHERE #{query}", *args).map do |row|
+      Chef.new(row)
+    end
+  end
+
+  def self.find(pk)
+    self.where('id = ?', pk).first
+  end
+  
+  def save
+    if new_record?
+      results = insert!
+    else
+      results = update!
     end
 
+    # When we save, remove changes between new and old attributes
+    @old_attributes = @attributes.dup
+
+    results
+  end
+    def new_record?
+    self[:id].nil?
+  end
+
+  def save
+    if new_record?
+      results = insert!
+    else
+      results = update!
+    end
+
+    # When we save, remove changes between new and old attributes
+    @old_attributes = @attributes.dup
+
+    results
+  end
+
+  # We say a record is "new" if it doesn't have a defined primary key in its
+  # attributes
+  def new_record?
+    self[:id].nil?
+  end
+
+  # e.g., chef[:first_name] #=> 'Steve'
+  def [](attribute)
+    raise_error_if_invalid_attribute!(attribute)
+
+    @attributes[attribute]
+  end
+
+  # e.g., chef[:first_name] = 'Steve'
+  def []=(attribute, value)
+    raise_error_if_invalid_attribute!(attribute)
+
+    @attributes[attribute] = value
+  end
+
+    #insert_1************************
     def self.inherited(klass)
     end
 
@@ -43,6 +97,7 @@ module MiniActiveRecord
       @connection
     end
 
+
     def self.execute(query, *args)
       raise NotConnectedError, "You are not connected to a database." unless connected?
 
@@ -53,80 +108,24 @@ module MiniActiveRecord
     def self.connected?
       !self.connection.nil?
     end
-#metodos insertados de otros archivos (meals & chefs)
-  def self.all
-    MiniActiveRecord::Model.execute("SELECT * FROM #{self}s").map do |row|
-      Meal.new(row)
-    end
-  end
 
-  def self.where(query, *args)
-    MiniActiveRecord::Model.execute("SELECT * FROM #{self}s WHERE #{query}", *args).map do |row|
-      Meal.new(row)
-    end
-  end
-
-def self.create(attributes)
-  record = self.new(attributes)
-  record.save
-
-  record
-end
-
-def self.find(pk)
-  self.where('id = ?', pk).first
-end
-
-  # e.g., chef[:first_name] #=> 'Steve'
-  def [](attribute)
-    raise_error_if_invalid_attribute!(attribute)
-
-    @attributes[attribute]
-  end
-
-  # e.g., chef[:first_name] = 'Steve'
-  def []=(attribute, value)
-    raise_error_if_invalid_attribute!(attribute)
-
-    @attributes[attribute] = value
-  end
-
-
-  def new_record?
-    self[:id].nil?
-  end
-
-
-  def save
-    if new_record?
-      results = insert!
-    else
-      results = update!
+    def self.attribute_names
+      @attribute_names
     end
 
-    # When we save, remove changes between new and old attributes
-    @old_attributes = @attributes.dup
+    def self.attribute_names=(attribute_names)
+      @attribute_names = attribute_names
+    end
 
-    results
-  end
+    def self.last_insert_row_id
+      MiniActiveRecord::Model.connection.last_insert_row_id
+    end
 
-  def self.attribute_names
-    @attribute_names
-  end
+    def valid_attribute?(attribute)
+      self.class.attribute_names.include? attribute
+    end
 
-  def self.attribute_names=(attribute_names)
-    @attribute_names = attribute_names
-  end
-
-  def self.last_insert_row_id
-    MiniActiveRecord::Model.connection.last_insert_row_id
-  end
-
-  def valid_attribute?(attribute)
-    self.class.attribute_names.include? attribute
-  end
-
-  def raise_error_if_invalid_attribute!(attributes)
+    def raise_error_if_invalid_attribute!(attributes)
       # This guarantees that attributes is an array, so we can call both:
       #   raise_error_if_invalid_attribute!("id")
       # and
@@ -144,48 +143,17 @@ end
     end
 
 
-  private
-      def self.prepare_value(value)
-        case value
-        when Time, DateTime, Date
-          value.to_s
-        else
-          value
-        end
+    private
+
+    def self.prepare_value(value)
+      case value
+      when Time, DateTime, Date
+        value.to_s
+      else
+        value
       end
+    end
 
-      def insert!
-        self[:created_at] = DateTime.now
-        self[:updated_at] = DateTime.now
-
-        fields = self.attributes.keys
-        values = self.attributes.values
-        marks  = Array.new(fields.length) { '?' }.join(',')
-
-        insert_sql = "INSERT INTO #{self}s (#{fields.join(',')}) VALUES (#{marks})" #<------ Problema
-        insert_sql = "INSERT INTO #{self}s (#{fields.join(',')}) VALUES (#{marks})"
-
-
-        results = MiniActiveRecord::Model.execute(insert_sql, *values)
-
-        # This fetches the new primary key and updates this instance
-        self[:id] = MiniActiveRecord::Model.last_insert_row_id
-        results
-      end
-
-      def update!
-        self[:updated_at] = DateTime.now
-
-        fields = self.attributes.keys
-        values = self.attributes.values
-
-        update_clause = fields.map { |field| "#{field} = ?" }.join(',')
-        update_sql = "UPDATE chefs SET #{update_clause} WHERE id = ?" #<------ Problema
-        update_sql = "UPDATE meals SET #{update_clause} WHERE id = ?"
-
-        # We have to use the (potentially) old ID attribute in case the user has re-set it.
-        MiniActiveRecord::Model.execute(update_sql, *values, self.old_attributes[:id])
-      end
   end
 
 end
